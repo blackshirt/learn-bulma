d59eeb66a07fa08d06de948ada67eb9d907ade94
blackshirt <blackshirtmuslim@yahoo.com>
2020-04-14 07:58:07 +0700

    2nd commit

---
+++ vue-dashboard/src/components/Home.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <img src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/036/square_256/vue.png">
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" @click="isOpen = !isOpen"
+                    v-bind:class="{'is-active': isOpen}">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div class="navbar-menu" v-bind:class="{'is-active': isOpen}">
+                <div class="navbar-end">
+                    <router-link to="/" class="navbar-item is-tab">Home</router-link>
+                    <router-link to="/about" class="navbar-item is-tab">About</router-link>
+                    <router-link to="/else" class="navbar-item is-tab">Something Else</router-link>
+                </div>
+            </div>
+        </nav>
+        <section class="hero is-info is-large">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        Large title
+                    </h1>
+                    <h2 class="subtitle">
+                        Large subtitle
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <footer class="footer">
+            <div class="container">
+                <div class="content has-text-centered">
+                    <p> {{msg}}
+                        <a class="icon" href="https://github.com/jgthms/bulma">
+                            <i class="fa fa-github"></i>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        props: {
+            msg: String
+        },
+        data() {
+            return {
+                isOpen: false
+            }
+        },
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ vue-dashboard/src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+import Dashboard from "../pages/Dashboard.vue";
+import Books from "../pages/Books.vue";
+import Customers from "../pages/Customers.vue";
+import Orders from "../pages/Orders.vue";
+import Home from "../components/Home.vue";
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [{
+            path: "/",
+            name: Home,
+            component: Home
+        },
+        {
+            path: "/dashboard",
+            name: Dashboard,
+            component: Dashboard
+        },
+        {
+            path: "/books",
+            name: Books,
+            component: Books
+        },
+        {
+            path: "/customers",
+            name: Customers,
+            component: Customers
+        },
+        {
+            path: "/orders",
+            name: Orders,
+            component: Orders
+        }
+    ],
+
+    linkActiveClass: "is-active"
+        /* change to Bulma's active nav link */
+});
